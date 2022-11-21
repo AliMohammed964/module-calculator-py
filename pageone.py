ec9757037c43dcb74050a983318ded9eb133377c
Ali M. Salman <ali.mohmaed2001@gmail.com>
2022-11-21 21:10:43 +0300

    change page 1 and 2

---
+++ pageone.py
@@ -0,0 +1,9 @@
+import pagetwo
+
+a = float(input("Enter first number"))
+b = float(input("Enter second number"))
+
+pagetwo.adding(a, b)
+pagetwo.subtracking(a, b)
+pagetwo.mutliplaying(a, b)
+pagetwo.div(a, b)
---
+++ pagetwo.py
@@ -0,0 +1,14 @@
+def adding(x, y):
+    print("Adding result = ", x + y)
+
+
+def subtracking(x, y):
+    print("Sub Result = ", x - y)
+
+
+def mutliplaying(x, y):
+    print("multi Result = ", x * y)
+
+
+def div(x, y):
+    print("div Result = ", x / y)
