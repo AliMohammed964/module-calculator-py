c95af3064fa958bfe176409d7323460142cf9a0c
Ali M. Salman <ali.mohmaed2001@gmail.com>
2022-11-22 19:36:29 +0300

    Add employee class

---
+++ employee.py
@@ -0,0 +1,30 @@
+class employee():
+    name = ''
+    age = 0
+    dept = ' '
+    phone = 0
+
+    def enterdata(self):
+        self.name = input("Enter your name : ")
+        self.age = int(input("Enter your age : "))
+        self.dept = input("Enter your department : ")
+        self.phone = input("Enter your phone : ")
+
+    def show(self):
+        print('name : ' + self.name + ' // age : ' + " " + str(self.age) + '//Department : ' + self.dept + '//Phone : ' + str(self.phone))
+
+    def compare(self, age):
+        if 18 < self.age < 30:
+            result = 'young'
+        elif self.age > 30:
+            result = 'old'
+        elif self.age < 18:
+            result = 'illegal age'
+
+        return result
+
+
+e1 = employee()
+e1.enterdata()
+e1.show()
+print(e1.compare(e1.age))
